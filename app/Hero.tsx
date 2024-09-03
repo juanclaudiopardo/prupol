87486e089c2d4ae008718bc0ef4e6ea1b5d60cc7
Juan Pardo <josefrecinosanitarios@yahoo.com.ar>
2024-09-03 17:14:34 -0300

    first commit

---
+++ app/Hero.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+export const Hero = () => {
+  return (
+    <section
+      id='#'
+      className='relative h-screen bg-[url("/obelisco.jpg")] bg-cover bg-center bg-no-repeat flex items-center justify-center'
+    >
+      <div className='absolute inset-0 bg-black opacity-50'></div>
+      <div className='relative z-10 container mx-auto px-4 text-center'>
+        <div className='max-w-3xl mx-auto'>
+          <h1 className='text-4xl md:text-6xl lg:text-7xl font-bold tracking-tighter text-white mb-6 '>
+            Prupol Seguridad
+          </h1>
+          <p className='text-lg md:text-xl text-white mb-8 max-w-2xl mx-auto'>
+            Fundada en 1967. Socios Fundadores de C.A.E.S.I.
+          </p>
+          <Link
+            href='#customers'
+            className='btn btn-primary text-lg px-8 py-3 transition duration-300 ease-in-out hover:border-yellow-500 hover:text-yellow-500'
+          >
+            Conocenos
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from './Header';
+import { Hero } from './Hero';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
   );
 }
